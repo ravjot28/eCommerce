7bb820049bef9382fbc4f7c43200e18e860f21d1
Marcelo Luna <luna.marceloandres@gmail.com>
2013-10-01 13:03:43 -0500

    ProductoRepository y configuracion de Hibernate en el CommerceController

--- ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/CommerceController.java
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/CommerceController.java
@@ -1,17 +1,33 @@
 package web.ecommerce.tpfinal.ecommerce_web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+
+
 @Controller
 @RequestMapping(value="/commerce/**")
 public class CommerceController {
+	
+	@Autowired
+	private ProductoRepository ProductoRepository;
 	
 	@RequestMapping(value="/index", method=RequestMethod.GET)
 	public ModelAndView index(){
 		ModelAndView mav = new ModelAndView();		
 		return mav;
 	}
+	@RequestMapping(value="/busquedaProductos", method=RequestMethod.GET)
+	public ModelAndView busquedaP(){
+		ModelAndView mav = new ModelAndView();
+		
+		mav.getModelMap().addAttribute("productos", ProductoRepository.findAll());
+		return mav;
+	}
+	
+	
+	
 }
--- ecommerce-web/src/main/webapp/WEB-INF/views/commerce/index.jsp
+++ ecommerce-web/src/main/webapp/WEB-INF/views/commerce/index.jsp
@@ -1,4 +1,5 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib uri="http://www.springframework.org/tags" prefix="s"%>
 
-<h1>Bienvenido</h1>
+<h1>Bienvenido a la Tienda Online!</h1>
+
