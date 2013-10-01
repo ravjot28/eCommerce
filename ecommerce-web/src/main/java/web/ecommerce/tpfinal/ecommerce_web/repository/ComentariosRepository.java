cc74a11565174ac20c9aa2fcf13bcd647dd7abc8
coresaxel <coresaxel@yahoo.com.ar>
2013-10-01 16:33:42 -0300

    agrego el ComentariosRepository

--- ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/repository/ComentariosRepository.java
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/repository/ComentariosRepository.java
@@ -1,10 +1,46 @@
 package web.ecommerce.tpfinal.ecommerce_web.repository;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import web.ecommerce.tpfinal.ecommerce_web.clasesDominio.Comentario;
+
 
 @Repository
 @Transactional(readOnly = true)
 public class ComentariosRepository {
+	
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<Comentario> findAll(){
+		TypedQuery<Comentario> q = entityManager.createQuery("select a from Comentario c", Comentario.class);
+		List<Comentario> comentarios = q.getResultList();
+		
+		return comentarios;
+	}
+	public void create(){
+		
+	}
+	public void block(){
+		
+	}
+	public void delete(){
+		
+	}
+	public void save(){
+		
+	}
+	public void get(){
+		
+	}
+	
 
 }
