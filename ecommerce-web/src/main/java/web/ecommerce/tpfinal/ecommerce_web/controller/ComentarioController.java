2a9aa0f83c56f0f7dd4851238a43f4454da2d65a
captton <captton@192.168.101.124>
2013-10-01 15:55:53 -0300

    agrege al controlaer del comentario

--- ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/ComentarioController.java
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/ComentarioController.java
@@ -1,5 +1,30 @@
 package web.ecommerce.tpfinal.ecommerce_web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 public class ComentarioController {
-	e.e
+	@Controller
+	@RequestMapping(value="/commerce/**")
+	public class CommerceController {
+		
+		@Autowired
+		private ProductoRepository ComentarioRepository;
+		
+		@RequestMapping(value="/mostrar", method=RequestMethod.GET)
+		public ModelAndView index(){
+			ModelAndView mav = new ModelAndView();		
+			return mav;
+		}
+		@RequestMapping(value="/busquedaProductos", method=RequestMethod.GET)
+		public ModelAndView busquedaP(){
+			ModelAndView mav = new ModelAndView();
+			
+			return mav;
+		}
+	}
 }
+
