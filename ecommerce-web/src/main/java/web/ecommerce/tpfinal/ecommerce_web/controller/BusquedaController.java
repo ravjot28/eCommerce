92d10c816564712c3c46cb08a2237e131f826355
Marcelo Luna <luna.marceloandres@gmail.com>
2013-10-01 14:09:51 -0500

    BusquedaController y modificacion CommerceController

--- ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/BusquedaController.java
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/BusquedaController.java
@@ -1,33 +1,19 @@
 package web.ecommerce.tpfinal.ecommerce_web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
-@Controller
-@RequestMapping(value="/commerce/**")
-public class CommerceController {
-	
+public class BusquedaController {
 	@Autowired
 	private ProductoRepository ProductoRepository;
 	
-	@RequestMapping(value="/index", method=RequestMethod.GET)
-	public ModelAndView index(){
-		ModelAndView mav = new ModelAndView();		
-		return mav;
-	}
 	@RequestMapping(value="/busqueda", method=RequestMethod.GET)
 	public ModelAndView busquedaP(){
 		ModelAndView mav = new ModelAndView();
-		
-		
+				
 		return mav;
 	}
-	
-	
 	
 }
