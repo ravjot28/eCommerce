5fa4ed47b5f900f7ec48b044a417735680b596d5
coresaxel <coresaxel@yahoo.com.ar>
2013-10-02 15:09:36 -0300

    Agrego nuevamente las clases Comentario y ComentarioRepository

---
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/clasesDominio/Comentario.java
@@ -0,0 +1,71 @@
+package web.ecommerce.tpfinal.ecommerce_web.clasesDominio;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Comentario {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private String texto;
+	
+	private transient IComentable comentable;
+	
+	@OneToOne
+	private Usuario usuario;
+	private boolean aceptado = true;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Comentario(String texto, IComentable comentable,
+			Usuario usuario, boolean aceptado) {
+
+		this.texto = texto;
+		this.comentable = comentable;
+		this.usuario = usuario;
+		this.aceptado = aceptado;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public IComentable getComentable() {
+		return comentable;
+	}
+
+	public void setComentable(IComentable comentable) {
+		this.comentable = comentable;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public boolean isAceptado() {
+		return aceptado;
+	}
+
+	public void setAceptado(boolean aceptado) {
+		this.aceptado = aceptado;
+	}
+}
---
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/repository/ComentariosRepository.java
@@ -0,0 +1,57 @@
+package web.ecommerce.tpfinal.ecommerce_web.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import web.ecommerce.tpfinal.ecommerce_web.clasesDominio.Comentario;
+//import web.ecommerce.tpfinal.ecommerce_web.controller.ComentarioController;
+
+
+@Repository
+@Transactional(readOnly = true)
+public class ComentariosRepository {
+	private static final Logger LOG = LoggerFactory.getLogger(ComentariosRepository.class);
+
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	public List<Comentario> findAll(int id){
+		TypedQuery<Comentario> q = entityManager.createQuery("select a from Comentario c", Comentario.class);
+		List<Comentario> comentarios = q.getResultList();
+		LOG.info("Se obtuvieron {} comentarios", comentarios.size());
+
+		return comentarios;
+	}
+	public void create(Comentario comentario){
+		entityManager.persist(comentario);
+	}
+	public void block(int numeroComentario){
+		if(get(numeroComentario).isAceptado()){
+			get(numeroComentario).setAceptado(false);
+		}else{
+			get(numeroComentario).setAceptado(true);
+		}
+	}
+	public void delete(int numeroComentario){
+		entityManager.remove(numeroComentario);
+	}
+	public Comentario save(Comentario comentario){
+		comentario.setTexto(comentario.getTexto());		
+		entityManager.persist(comentario);
+		return null;
+
+	}
+	public Comentario get(int numeroComentario){
+		Comentario comentarios = entityManager.find(Comentario.class, numeroComentario);
+		return comentarios;
+	}
+}
+
