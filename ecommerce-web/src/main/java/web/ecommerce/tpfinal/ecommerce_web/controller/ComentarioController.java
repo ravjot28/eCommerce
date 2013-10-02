7152ca4f5920ebb5449f056f36cee3fc23b85b19
captton <captton@192.168.101.124>
2013-10-02 15:09:10 -0300

    backup de controller comentario

---
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/ComentarioController.java
@@ -0,0 +1,77 @@
+package web.ecommerce.tpfinal.ecommerce_web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import web.ecommerce.tpfinal.ecommerce_web.clasesDominio.Comentario;
+import web.ecommerce.tpfinal.ecommerce_web.repository.ComentariosRepository;
+
+@Controller
+@RequestMapping(value="/comentarios/**")
+
+public class ComentarioController {
+
+	@Autowired
+	private ComentariosRepository comentariosRepository;
+
+	@RequestMapping(value="/comentario", method=RequestMethod.GET)
+	public ModelAndView comentario(@RequestParam("id") int id, @RequestParam("nombre") String nombre){
+		ModelAndView mav = new ModelAndView();
+		mav.getModelMap().addAttribute("comentarios", comentariosRepository.findAll(id));
+		mav.getModelMap().addAttribute("nombre", nombre);
+		mav.getModelMap().addAttribute("idComentable", id);
+		return mav;
+	}
+		
+	@RequestMapping(value="/mostrar", method=RequestMethod.GET)
+	public ModelAndView mostrar(@RequestParam("id") int id, @RequestParam("nombre") String nombre){
+		ModelAndView mav = new ModelAndView();
+		mav.getModelMap().addAttribute("comentarios", comentariosRepository.findAll());
+		mav.getModelMap().addAttribute("nombre", nombre);
+		return mav;
+	}
+
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public ModelAndView add(@ModelAttribute Comentario unComentario){
+		ModelAndView mav = new ModelAndView("redirect:comentario?id={}&nombre={}" ,Integer.toString(unComentario.getComentable().getID()), unComentario.getComentable().getNombre());
+		comentariosRepository.create(unComentario);
+		return mav;
+	}
+
+	@RequestMapping(value="/block", method=RequestMethod.GET)
+	public ModelAndView block(@RequestParam("numeroComentario") int numeroComentario){
+		ModelAndView mav = new ModelAndView();
+		comentariosRepository.block(numeroComentario);
+		return mav;
+	}
+
+	@RequestMapping(value="/remove", method=RequestMethod.GET)
+	public ModelAndView remove(@RequestParam("numeroComentario") int numeroComentario){
+		ModelAndView mav = new ModelAndView();
+		comentariosRepository.delete(numeroComentario);
+		return mav;
+	}
+
+	@RequestMapping(value="/edit", method=RequestMethod.GET)
+	public ModelAndView edit(@RequestParam("numeroComentario") int numeroComentario){
+		ModelAndView mav = new ModelAndView();
+		Comentario elComentario = comentariosRepository.get(numeroComentario);
+		mav.getModelMap().addAttribute("comentario", elComentario);
+		return mav;
+	}
+
+	@RequestMapping(value="/edit", method=RequestMethod.POST)
+	public ModelAndView edit(@ModelAttribute Comentario unComentario){
+		ModelAndView mav = new ModelAndView();
+		comentariosRepository.save(unComentario);
+		return mav;
+	}
+
+}
+
+
