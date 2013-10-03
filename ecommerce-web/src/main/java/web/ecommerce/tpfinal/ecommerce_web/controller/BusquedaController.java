cde12e2771b1fbbe70c4c05e2bef56ea71db5097
xp <xp@192.168.101.103>
2013-10-03 16:20:31 -0300

    controller funcionando

--- ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/BusquedaController.java
+++ ecommerce-web/src/main/java/web/ecommerce/tpfinal/ecommerce_web/controller/BusquedaController.java
@@ -3,7 +3,6 @@ package web.ecommerce.tpfinal.ecommerce_web.controller;
 import java.util.ArrayList;
 
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
